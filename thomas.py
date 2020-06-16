edd7fb7f4a5d8e39c7c8926de781f804ba49d832
thomasboulanger <60351261+thomasboulanger@users.noreply.github.com>
2020-06-16 12:16:36 +0200

    +1

--- thomas.py
+++ thomas.py
@@ -1,9 +1,7 @@
 import random
 
-
 pendu = open("C:\\Users\\tomal\\OneDrive\Bureau\\partiel thomas\\Partiel_B1Game_S2_Python\\pendu.txt", "r")
 liste = open("C:\\Users\\tomal\\OneDrive\Bureau\\partiel thomas\\Partiel_B1Game_S2_Python\\dict.txt", "r")
-
 
 new_pendu = pendu.read()
 new_liste = liste.read()
@@ -19,43 +17,28 @@ mot = random.choice(new_liste)
 mot_en_lettre = list(mot)
 vide = []
 
-print(mot_en_lettre)
-
 print("mot choisi , il a {} lettres a toi de les trouver !\n".format(len(mot)))
 print(mot)
-
-
 
 for i in range(len(mot)):
         print("_ ")  # a voir plus tard le saut de ligne...
 while win != True and compteur <= 8:
     if compteur >=8:
-        print("c est perdu !")
+        print("c est perdu ! (c etait ",mot,")...")
         break
-    if mot_en_lettre == vide:        #je voulais faire une liste de chaques lettres du mot choisi et les retirer quand le joueur les annonces , et si la liste est vide , win 
+    if mot_en_lettre == vide:
         print("gg wp , t as gagné")
         win = True   #meme si  c est aps utile car break ... :v
         break
-    lettre = input("rentre une seule lettre majuscule :   ")
-    if lettre < 'A' and lettre >'Z' :
-        continue
+    lettre = input("rentre une seule lettre seulement :   ")
+    lettre = lettre.upper()
+    if chr(48)<= lettre <= chr(57) :   #ne prend pas les cas de multiple lettres...
+        print("non , tu dois mettre une seule LETTRE ! ")
     else:
         if (mot.find(lettre) != -1):
             print ("le mot contien bien la lettre ",lettre)
+            mot_en_lettre.remove(lettre)
         else:
-            print ("ta lettre n est pas dans le mot , essai encore...")
+            print ("ta lettre n est pas dans le mot...")
+            print(new_pendu[compteur])
             compteur +=1
-    
-   
-    
-    
-
-
-#print(new_liste)
-#print(new_pendu)
-
-
-
-
-
-
